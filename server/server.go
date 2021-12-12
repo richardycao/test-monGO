ba5b84ecf666d358ef3341d3389a97c031e3dd17
richardycao <richardycao@gmail.com>
2021-12-12 23:12:31 +0000

    Set up port for mongo. Set up poll apis

--- server/server.go
+++ server/server.go
@@ -10,14 +10,18 @@ import (
 
 	"github.com/gorilla/mux"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+
 	// "go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 type Poll struct {
-	ID			primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
-	Title 		string             `json:"title,omitempty" bson:"title,omitempty"`
+	ID    primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Title string             `json:"title,omitempty" bson:"title,omitempty"`
 }
+
 var client *mongo.Client
 
 func DefaultEndpoint(response http.ResponseWriter, request *http.Request) {
@@ -25,30 +29,71 @@ func DefaultEndpoint(response http.ResponseWriter, request *http.Request) {
 }
 
 func CreatePollEndpoint(response http.ResponseWriter, request *http.Request) {
-	fmt.Println("1")
 	response.Header().Add("content-type", "application/json")
-	fmt.Println("2")
 	var poll Poll
 	json.NewDecoder(request.Body).Decode(&poll)
-	fmt.Println("3")
-	fmt.Println(poll.ID, poll.Title)
 	collection := client.Database("myDB").Collection("polls")
-	fmt.Println("4")
 	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	fmt.Println("5")
-	result, _ := collection.InsertOne(ctx, poll)
-	fmt.Println("6")
+	result, err := collection.InsertOne(ctx, poll)
+	if err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{ "message": "` + err.Error() + `" }`))
+		return
+	}
 	json.NewEncoder(response).Encode(result)
+	fmt.Println(result)
 }
 
-func main() {
+func GetPollEndpoint(response http.ResponseWriter, request *http.Request) {
+	response.Header().Add("content-type", "application/json")
+	params := mux.Vars(request)
+	id, _ := primitive.ObjectIDFromHex(params["id"])
+	var poll Poll
+	collection := client.Database("myDB").Collection("polls")
 	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	client, _ = mongo.Connect(ctx, "mongodb://localhost:7017")
+	err := collection.FindOne(ctx, Poll{ID: id}).Decode(&poll)
+	if err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{ "message": "` + err.Error() + `" }`))
+		return
+	}
+	json.NewEncoder(response).Encode(poll)
+	fmt.Println(poll, poll.ID, poll.Title)
+}
+
+// https://stackoverflow.com/questions/54778520/mongo-go-driver-failing-to-connect
+func ConnectMongo() {
+	var (
+		client     *mongo.Client
+		mongoURL = "mongodb://localhost:27017"
+	)
+ 
+	// Initialize a new mongo client with options
+	client, err := mongo.NewClient(options.Client().ApplyURI(mongoURL))
+ 
+	// Connect the mongo client to the MongoDB server
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+ 
+	// Ping MongoDB
+	ctx, _ = context.WithTimeout(context.Background(), 10*time.Second)
+	if err = client.Ping(ctx, readpref.Primary()); err != nil {
+		fmt.Println("could not ping to mongo db service: %v\n", err)
+		return
+	}
+ 
+	fmt.Println("Connected to nosql database:", mongoURL)
+ 
+ }
+
+func main() {
+	ConnectMongo()
 
 	router := mux.NewRouter()
 	const port string = ":8080"
 	router.HandleFunc("/", DefaultEndpoint).Methods("GET")
 	router.HandleFunc("/poll", CreatePollEndpoint).Methods("POST")
+	router.HandleFunc("/poll/{id}", CreatePollEndpoint).Methods("GET")
 
 	log.Print("Server listening on port ", port)
 	log.Fatalln(http.ListenAndServe(port, router))
